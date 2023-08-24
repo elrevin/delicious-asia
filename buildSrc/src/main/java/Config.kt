b3ac10e2e1e4f1ed973288caa4b156cc9891e0c4
elrevin <td@index-art.ru>
2023-08-24 23:46:25 +0600

    Create buildSrc folder

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,16 @@
+object Config {
+    const val packageName = "me.elrevin.deliciousasia"
+
+    const val compileSdk = 34
+    const val minSdk = 26
+    const val targetSdk = 34
+
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    const val hiltPluginVlasspath = "com.google.dagger:hilt-android-gradle-plugin:${Dependecies.Hilt.version}"
+
+    const val hiltPlugin = "com.google.dagger.hilt.android"
+}
---
+++ buildSrc/src/main/java/Dependecies.kt
@@ -0,0 +1,37 @@
+class Dependecies {
+    object Compose {
+        const val bom = "androidx.compose:compose-bom:2023.08.00"
+
+        const val ui = "androidx.compose.ui:ui"
+        const val graphics = "androidx.compose.ui:ui-graphics"
+        const val material = "androidx.compose.material3:material3"
+        const val tooling = "androidx.compose.ui:ui-tooling"
+        const val preview = "androidx.compose.ui:ui-tooling-preview"
+    }
+
+    object Lifecycle {
+        private const val lifecycleVersion = "2.6.1"
+        private const val composeViewModelVersion = "2.4.0-rc01"
+        private const val activityVersion = "1.7.2"
+
+        const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion"
+        const val lifecycleViewmodel = "androidx.lifecycle:lifecycle-viewmodel-compose:$composeViewModelVersion"
+        const val activity = "androidx.activity:activity-compose:$activityVersion"
+    }
+
+    object Other {
+        private const val coreVersion = "1.10.1"
+
+        const val core = "androidx.core:core-ktx:$coreVersion"
+    }
+
+    object Hilt {
+        const val version = "2.47"
+        private const val navigationVersion = "1.0.0"
+
+        const val hilt = "com.google.dagger:hilt-android:$version"
+        const val kapt = "com.google.dagger:hilt-android-compiler:$version"
+        const val navigation = "androidx.hilt:hilt-navigation-compose:$navigationVersion"
+    }
+
+}
